f8d9cee63bb03aa7bbb53347d9b67092b16ab209
Lucas252291 <lucas.jlrs252291@gmail.com>
2025-05-03 19:35:13 -0300

    Relation OneToOne

--- src/main/java/com/bookstore/jpa/models/BookModel.java
+++ src/main/java/com/bookstore/jpa/models/BookModel.java
@@ -31,6 +31,9 @@ public class BookModel implements Serializable {
                inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<AuthorModel> authors = new HashSet<>();
 
+    @OneToOne(mappedBy = "book", cascade = CascadeType.ALL)
+    private ReviewModel review;
+
     public UUID getId() {
         return id;
     }
@@ -61,5 +64,13 @@ public class BookModel implements Serializable {
 
     public void setAuthors(Set<AuthorModel> authors) {
         this.authors = authors;
+    }
+
+    public ReviewModel getReview() {
+        return review;
+    }
+
+    public void setReview(ReviewModel review) {
+        this.review = review;
     }
 }
